f4ac9bb2d85ba7c83ab4674a66ca47b59b18eb3b
Theo <118967935+FalcoBo@users.noreply.github.com>
2024-04-07 01:26:56 +0200

    fix app, add cleaned_csv

--- app.py
+++ app.py
@@ -1,38 +1,44 @@
 import streamlit as st
-import joblib
 import pandas as pd
+import matplotlib.pyplot as plt
+import joblib
 
 # Load the model
-model = joblib.load('./model/model_titanic.pkl')
+model = joblib.load('./model/SVC.pkl')
+cleaned_titanic = pd.read_csv('./data/cleaned_titanic.csv')
 
 def main():
-    st.title('Mon application Streamlit')
+    st.title('Prédire les Surviants du Titanic')
+    st.write('Veuillez remplir les informations suivantes :')
 
-    st.write('Bienvenue ! Utilisez cette application pour faire des prédictions.')
-
-    # Add input fields for user input
-    sex = st.selectbox('Sexe', ['male', 'female'])
+    sex = st.radio('Sexe', ['male', 'female'])
     age = st.slider('Âge', min_value=0, max_value=100, value=30, step=1)
     pclass = st.selectbox('Classe', [1, 2, 3])
     fare = st.number_input('Fare', value=50.0)
-    embarked = st.selectbox('Port d\'embarquement', ['C', 'Q', 'S'])
+    embarked = st.radio('Port d\'embarquement', [0, 1, 2])
 
-    # Prepare input data with correct column order
-    input_data = pd.DataFrame({'Sex': [sex], 'Age': [age], 'Pclass': [pclass], 'Fare': [fare], 'Embarked': [embarked], 'Parch': [0], 'SibSp': [0]}, columns=['Sex', 'Age', 'Pclass', 'Fare', 'Embarked', 'Parch', 'SibSp'])
+    prediction = model.predict(cleaned_titanic)
 
-    # Make prediction
-    prediction = model.predict(input_data)
-    prediction_proba = model.predict_proba(input_data)
-
-    # Display prediction
     if prediction[0] == 0:
         st.write('Le passager est prédit comme non survivant.')
     else:
         st.write('Le passager est prédit comme survivant.')
 
-    # Display prediction probability
-    st.write(f'Probabilité de non survie: {prediction_proba[0][0]:.2f}')
-    st.write(f'Probabilité de survie: {prediction_proba[0][1]:.2f}')
+    # Visualiser un graphique en temps réel basé sur les données utilisateur
+    df_input_data = pd.DataFrame({'Sex': [sex], 'Age': [age], 'Pclass': [pclass], 'Fare': [fare], 'Embarked': [embarked]})
+    st.write('Données utilisateur :')
+    st.write(df_input_data)
+
+    st.write('Visualisation en temps réel des données utilisateur :')
+    visualize_data(df_input_data)
+
+def visualize_data(data):
+    fig, ax = plt.subplots()
+    data.plot(kind='bar', ax=ax)
+    ax.set_title('Données utilisateur')
+    ax.set_xlabel('Caractéristiques')
+    ax.set_ylabel('Valeurs')
+    st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
