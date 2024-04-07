8d404377f04caaa9b98afe71799f5aa608e471f1
Theo <118967935+FalcoBo@users.noreply.github.com>
2024-04-07 21:52:58 +0200

    fix app, add class plots_predicts

--- app.py
+++ app.py
@@ -9,41 +9,45 @@ from plots_predict import PlotPredict
 model = joblib.load('./model/SVC.pkl')
 
 # Load the cleaned Titanic dataset
-cleaned_titanic = pd.read_csv('./data/cleaned_titanic.csv').drop('Survived', axis=1)
+cleaned_titanic = pd.read_csv('./data/cleaned_titanic.csv')
 
 # Function to display the main page
-def main():
+def main(include_survived=True):
     st.title('Prédire les Survivants du Titanic')
     st.write('Veuillez remplir les informations suivantes :')
 
-    plots = PlotPredict(cleaned_titanic, model.predict(cleaned_titanic))
+    if include_survived:
+        plots = PlotPredict(cleaned_titanic, model.predict(cleaned_titanic.drop('Survived', axis=1)))
+    else:
+        plots = PlotPredict(cleaned_titanic.drop('Survived', axis=1), model.predict(cleaned_titanic.drop('Survived', axis=1)))
 
     sex = st.radio('Sexe', ['male', 'female'])
     age = st.slider('Âge', min_value=0, max_value=100, value=30, step=1)
     pclass = st.selectbox('Classe', [1, 2, 3])
     fare = st.number_input('Fare', value=50.0)
     parch = st.number_input('Parch', value=0)
-    sibsp = st.number_input('SibSp', value=0)
+    SibSp = st.number_input('SibSp', value=0)
     embarked = st.radio('Port d\'embarquement', [0, 1, 2])
 
-    prediction = model.predict(cleaned_titanic)
+    if include_survived:
+        df_input_data = pd.DataFrame({'Sex': [sex], 'Age': [age], 'Pclass': [pclass], 'Fare': [fare], 'Parch':[parch], 'SibSp':[SibSp] , 'Embarked': [embarked], 'Survived': [1]})
+    else:
+        df_input_data = pd.DataFrame({'Sex': [sex], 'Age': [age], 'Pclass': [pclass], 'Fare': [fare], 'Parch':[parch], 'SibSp':[SibSp] , 'Embarked': [embarked]})
 
-    # Display the input data
-    df_input_data = pd.DataFrame({'Sex': [sex], 'Age': [age], 'Pclass': [pclass], 'Fare': [fare], 'Parch':[parch], 'Sibsp':[sibsp] , 'Embarked': [embarked]})
     st.write('Données utilisateur :')
     st.write(df_input_data)
 
     st.write('Visualisation en temps réel des données utilisateur :')
     plots.visualize_data(df_input_data)
 
-    if prediction[0] == 0:
+    if model.predict(df_input_data.drop('Survived', axis=1))[0] == 0:
         st.write('Le passager est prédit comme non survivant.')
     else:
         st.write('Le passager est prédit comme survivant.')
 
-    # Plot survived passengers with PassengerId
-    # st.write('Visualisation des passagers survivants avec leur PassengerId :')
-    # plots.plot_survived_passengers()
+    # Plot survived passengers with PassengerId by class
+    st.write('Visualisation des passagers survivants avec leur PassengerId par classe :')
+    plots.by_class(df_input_data)
 
 if __name__ == '__main__':
     main()
--- plots_predict.py
+++ plots_predict.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
+# Class to plot the predictions
+
 class PlotPredict:
     def __init__(self, cleaned_titanic, predictions):
         self.cleaned_titanic = cleaned_titanic
         self.predictions = predictions
 
+    # Method to visualize the data
     def visualize_data(self, data):
         fig, ax = plt.subplots()
         data.plot(kind='bar', ax=ax)
@@ -16,6 +19,7 @@ class PlotPredict:
         ax.set_ylabel('Valeurs')
         st.pyplot(fig)
 
+    # Method to plot the survived passengers with their PassengerId
     def plot_survived_passengers(self):
         survived_passengers = self.cleaned_titanic[self.predictions == 1]
         survived_passengers_df = pd.DataFrame(survived_passengers)
@@ -25,67 +29,100 @@ class PlotPredict:
         plt.title('Passagers survivants avec leur PassengerId')
         plt.show()
 
+    # Method to plot the survived passengers with their PassengerId by class
     def by_class(self, data):
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='Pclass', palette='deep', marker='o')
+        if 'Survived' in data.columns:
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Pclass', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Classe')
         plt.title('Passagers survivants avec leur PassengerId par classe')
         plt.show()
-    
+
+    # Method to plot the survived passengers with their PassengerId by age
     def by_age(self, data):
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='Age', palette='deep', marker='o')
+        if 'Survived' in data.columns:
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Age', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Âge')
         plt.title('Passagers survivants avec leur PassengerId par âge')
         plt.show()
 
+    # Method to plot the survived passengers with their PassengerId by sex
     def by_sex(self, data):
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='Sex', palette='deep', marker='o')
+        if 'Survived' in data.columns:
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Sex', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Sexe')
         plt.title('Passagers survivants avec leur PassengerId par sexe')
         plt.show()
 
+    # Method to plot the survived passengers with their PassengerId by fare
     def by_fare(self, data):
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='Fare', palette='deep', marker='o')
+        if 'Survived' in data.columns:
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Fare', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Tarif')
         plt.title('Passagers survivants avec leur PassengerId par tarif')
         plt.show()
 
+    # Method to plot the survived passengers with their PassengerId by Parch
     def by_parch(self, data):
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='Parch', palette='deep', marker='o')
+        if 'Survived' in data.columns:
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Parch', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Parch')
         plt.title('Passagers survivants avec leur PassengerId par Parch')
         plt.show()
 
-    def by_sibsp(self, data):
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='Sibsp', palette='deep', marker='o')
+    # Method to plot the survived passengers with their PassengerId by Sibsp
+    def by_Sibsp(self, data):
+        if 'Survived' in data.columns:
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Sibsp', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Sibsp')
         plt.title('Passagers survivants avec leur PassengerId par Sibsp')
         plt.show()
 
+    # Method to plot the survived passengers with their PassengerId by embarked
     def by_embarked(self, data):
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='Embarked', palette='deep', marker='o')
+        if 'Survived' in data.columns:
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Embarked', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Embarked')
         plt.title('Passagers survivants avec leur PassengerId par port d\'embarquement')
         plt.show()
-    
+
+    # Method to plot the survived passengers with their PassengerId by family size
     def by_family_size(self, data):
-        data['FamilySize'] = data['SibSp'] + data['Parch']
-        survived_passengers = data[self.predictions == 1]
-        sns.scatterplot(data=survived_passengers, x='PassengerId', y='Survived', hue='FamilySize', palette='deep', marker='o')
+        if 'Survived' in data.columns:
+            data['FamilySize'] = data['Sibsp'] + data['Parch']
+            survived_passengers = data[data['Survived'] == 1]
+        else:
+            data['FamilySize'] = data['Sibsp'] + data['Parch']
+            survived_passengers = data
+        sns.scatterplot(data=survived_passengers, x='PassengerId', y='FamilySize', palette='deep', marker='o')
         plt.xlabel('PassengerId')
-        plt.ylabel('Survived')
+        plt.ylabel('Taille de famille')
         plt.title('Passagers survivants avec leur PassengerId par taille de famille')
         plt.show()
